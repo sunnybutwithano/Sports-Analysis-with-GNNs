57d7e64c77fb1b1d181b51d845a1a7584b7b0340
indirected <46082152+indirected@users.noreply.github.com>
2022-01-12 15:19:28 +0330

    Populate play lists, Dataframe to node text - DL probably done

--- DataLoader.py
+++ DataLoader.py
@@ -26,7 +26,8 @@ class DataLoader:
         )
 
         self._data_cleanup()
-
+        self._gen_entities()
+        self._populate_play_lists()
 
 
 
@@ -49,6 +50,7 @@ class DataLoader:
         ]
         self.dataset = self.dataset.drop(corrupted.index, axis=0).reset_index(drop= True)
 
+
     #Converts a dataframe of dataset to numpy array
     def DatasetDataframetoNumpy(self, dataframe: pd.DataFrame):
         hteams = np.array(dataframe[self.HOME_TEAM_KEY].tolist())
@@ -57,13 +59,14 @@ class DataLoader:
         aplayers = np.array([lineup for lineup in dataframe[self.AWAY_LINEUP_KEY].tolist()])
         return hteams, ateams, hplayers, aplayers
 
+
     def _gen_entities(self):
         nodes = self.DatasetDataframetoNumpy(self.dataset)
         self.entities: np.ndarray = np.unique(np.concatenate((
             nodes[0],
             nodes[1],
-            np.char.strip(self.nodes[2].reshape(-1), '123456789., '),
-            np.char.strip(self.nodes[3].reshape(-1), '123456789., ')
+            np.char.strip(nodes[2].reshape(-1), '123456789., '),
+            np.char.strip(nodes[3].reshape(-1), '123456789., ')
         )))
         #Giving each entity an ID
         self.labeler = skp.LabelEncoder().fit(self.entities)
@@ -75,8 +78,61 @@ class DataLoader:
         self.team_play_list = {t:[] for t in teams}
 
         players = np.unique(np.concatenate((
-            np.char.strip(self.nodes[2].reshape(-1), '123456789., '),
-            np.char.strip(self.nodes[3].reshape(-1), '123456789., ')
+            np.char.strip(nodes[2].reshape(-1), '123456789., '),
+            np.char.strip(nodes[3].reshape(-1), '123456789., ')
         )))
         self.player_play_list = {p:[] for p in players}
 
+
+    def _populate_play_lists(self):
+        hteams = self.dataset[self.HOME_TEAM_KEY]
+        for index, t in hteams.iteritems():
+            self.team_play_list[t].append(index)
+        
+        ateams = self.dataset[self.AWAY_TEAM_KEY]
+        for index, t in ateams.iteritems():
+            self.team_play_list[t].append(index)
+
+        hplayers = self.dataset[self.HOME_LINEUP_KEY].apply(
+            lambda z: list(map(
+                lambda y: y.strip('123456789., '), z
+            ))
+        )
+        for index, plist in hplayers.iteritems():
+            for p in plist:
+                self.player_play_list[p].append(index)
+
+        aplayers = self.dataset[self.AWAY_LINEUP_KEY].apply(
+            lambda z: list(map(
+                lambda y: y.strip('123456789., '), z
+            ))
+        )
+        for index, plist in aplayers.iteritems():
+            for p in plist:
+                self.player_play_list[p].append(index)
+
+        #Sort the lists
+        for t in self.team_play_list.keys():
+            self.team_play_list[t] = np.sort(self.team_play_list[t])
+
+        for p in self.player_play_list.keys():
+            self.player_play_list[p] = np.sort(self.player_play_list[p])
+
+
+    def DatasetDataframetoNodeText(self, dataframe: pd.DataFrame):
+        hteams = np.array([f'{hteam}*{index}' for index, hteam in dataframe[self.HOME_TEAM_KEY].iteritems()])
+        ateams = np.array([f'{ateam}*{index}' for index, ateam in dataframe[self.AWAY_TEAM_KEY].iteritems()])
+
+        hplayers = dataframe[self.HOME_LINEUP_KEY].apply(lambda z: list(map(lambda y: y.strip('123456789., '), z)))
+        hplayers = np.array([
+            [f'{player}@{index}' for player in players] 
+                for index, players in hplayers.iteritems()
+        ])
+
+        aplayers = dataframe[self.AWAY_LINEUP_KEY].apply(lambda z: list(map(lambda y: y.strip('123456789., '), z)))
+        aplayers = np.array([
+            [f'{player}@{index}' for player in players] 
+                for index, players in aplayers.iteritems()
+        ])
+
+        return hteams, ateams, hplayers, aplayers
